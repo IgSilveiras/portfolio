e3f41141c217c2fb363981a886a98b2f60e48d2e
Nacho <IgnacioSilveira2091@gmail.com>
2025-02-22 20:27:21 -0300

    estilos nav y clase active dinamica

--- js/scrollBtn.js
+++ js/scrollBtn.js
@@ -1,9 +1,10 @@
+// Desplazamiento con los botones de la nav y landing
 document.querySelectorAll("[data-scroll-to]").forEach(element => {
 
     element.addEventListener("click", (e) => {
         e.preventDefault();
 
-        const targetId = event.currentTarget.getAttribute("data-scroll-to");
+        const targetId = e.currentTarget.getAttribute("data-scroll-to");
         const targetElement = document.getElementById(targetId);
 
         // Offset para evitar que el target quede debajo del header
@@ -17,3 +18,37 @@ document.querySelectorAll("[data-scroll-to]").forEach(element => {
     })
 
 })
+
+// Agregarle clase "active" al boton correspondiente a cada sección
+const sections = document.querySelectorAll("section");
+const observerOptions = {
+    root: null,
+    threshold: 0.5,
+};
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            const targetId = entry.target.getAttribute("id");
+
+            document.querySelectorAll("[data-scroll-to]").forEach(btn => {
+
+                if (btn.id !== "moreBtn") {              // Evito el boton de la landing
+
+                    if (btn.getAttribute("data-scroll-to") === targetId) {
+                        btn.classList.add("active");
+                    }
+
+                    else {
+                        btn.classList.remove("active")
+                    }
+                }
+            });
+        }
+    });
+
+}, observerOptions);
+
+sections.forEach(section => observer.observe(section));
