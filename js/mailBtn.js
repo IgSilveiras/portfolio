4c5e880d3c0a4b0c97fdbead0f7e95fba1dba45c
Nacho <IgnacioSilveira2091@gmail.com>
2026-09-06 00:47:07 -0300

    mejoras para los lectores de pantalla y accesibilidad

--- js/mailBtn.js
+++ js/mailBtn.js
@@ -3,6 +3,9 @@ document.getElementById("mailLink").addEventListener("click", () => {
     navigator.clipboard.writeText("IgSilveiras21@gmail.com");
 
     const toast = document.getElementById("copy-toast");
+    toast.textContent = translations[language]["contact.mailToast"];
     toast.classList.add("show");
+
     setTimeout(() => toast.classList.remove("show"), 2500);
+    setTimeout(() => toast.textContent = "", 2800)
 })
